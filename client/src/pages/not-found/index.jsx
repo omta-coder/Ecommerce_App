e0bbe52a6db92f47fee3f712140539a546a36b03
Omta <omtadayma2001@gmail.com>
2024-09-02 17:33:13 +0530

    Routing || Pages || Components

---
+++ client/src/pages/not-found/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="max-h-screen w-full flex justify-center items-center">
+      <img 
+        className="w-full md:w-1/2 lg:w-1/3 xl:w-1/2 mx-auto" 
+        src="./error.jpg" 
+        alt="" 
+      />
+    </div>
+  );
+};
+
+export default NotFound;
