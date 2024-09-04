c49797698341688886857ba7a25ce10f31dad023
Omta <omtadayma2001@gmail.com>
2024-09-04 21:18:13 +0530

    Admin Panel

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
+import { AlignJustify, LogOut } from "lucide-react";
 
-const AdminHeader = () => {
+const AdminHeader = ({setOpen}) => {
   return (
-    <div>AdminHeader</div>
-  )
-}
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button onClick={()=>setOpen(true)} className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className="sr-only">Toggle menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
+};
 
-export default AdminHeader
+export default AdminHeader;
